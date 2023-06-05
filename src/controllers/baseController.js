b8d9a92ffa413d4477246e4d191fdda621f0182e
kirylzavarotny <kirillzavorotni@gmail.com>
2023-06-05 21:45:49 +0200

    Add puppeeter functional to go to page

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -4,7 +4,7 @@ const { logResponse } = require('../services/logger/logger');
 exports.baseController = async (req, res, next) => {
     try {
         logResponse(req, res);
-        res.status(200).json(await getUrlSummary());
+        res.status(200).json(await getUrlSummary(req.body.url));
     } catch (err) {
         next(err);
     }
--- src/services/baseService.js
+++ src/services/baseService.js
@@ -1,55 +1,41 @@
 /* eslint-disable */
 
-// const puppeteer = require('puppeteer');
-
 // const LANGUAGE = 'русском';
 //
 // const PROMPT = ''
 //     + `Дай менее детальную выжимку на ${LANGUAGE} языке из этого текста по каждому из логически отделенного раздела этого текста и `
 //     + 'c необходимыми подробностями и с выделенем основных (возможно важных) элементов в этом тексте:';
 
-const { SystemError } = require('../services/error/errorModule');
+const puppeteer = require('puppeteer');
+const { SystemError } = require('./error/errorModule');
 
-const getUrlSummary = async () => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve('done');
-        }, 500);
+const getUrlSummary = async (url) => {
+    console.log('url = ', url);
+
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null,
+    });
+    const [page] = await browser.pages();
+    await page.setViewport({ width: 1920, height: 1080 });
+
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    page.on('error', (err) => {
+        browser.close();
+        throw new SystemError(`Page error while try to go: "${url}"`, err);
     });
 
-    // try {
-    //     const browser = await puppeteer.launch({
-    //         headless: false,
-    //         defaultViewport: null,
-    //     });
-    //     const [page] = await browser.pages();
-    //     await page.setViewport({ width: 1920, height: 1080 });
-    //
-    //     await page.goto(url, { waitUntil: "domcontentloaded" });
-    //
-    //     page.on('error', (err) => {
-    //         console.error('Page error:', err);
-    //         browser.close();
-    //         res.sendStatus(500);
-    //     });
-    //
-    //     page.on('pageerror', (err) => {
-    //         console.error('Page error:', err);
-    //         browser.close();
-    //         res.sendStatus(500);
-    //     });
-    //
-    //     const textContent = await page.evaluate(() => {
-    //         return document.querySelector('body').innerText;
-    //     });
-    //
-    //     await browser.close();
-    //
-    //     res.json({ textContent });
-    // } catch (err) {
-    //     console.error('Error:', err);
-    //     res.sendStatus(500);
-    // }
+    page.on('pageerror', (err) => {
+        browser.close();
+        throw new SystemError(`Page error while try to go: "${url}"`, err);
+    });
+
+    const textContent = await page.evaluate(() => document.querySelector('body').innerText);
+
+    await browser.close();
+
+    return textContent;
 };
 
 module.exports = {
